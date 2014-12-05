dc66a7f01560d0b1012ffe2ebf6ed4e98ee5e1fb
Jesse MacFadyen <purplecabbage@gmail.com>
2014-12-04 16:38:04 -0800

    CB-7892 XHR to local files poly should only load/run once
    (cherry picked from commit 6bb3fc38a4d67cf378e1b11839c4af327ebfccd5)

--- wp8/template/cordovalib/XHRHelper.cs
+++ wp8/template/cordovalib/XHRHelper.cs
@@ -23,6 +23,9 @@ namespace WPCordovaClassLib.CordovaLib
     if (!win.__XHRShimAliases) {
         win.__XHRShimAliases = {};
     }
+    else {
+        return; // already initialized, this happens when navigating to #
+    }
 
     win.__onXHRLocalCallback = function (responseCode, responseText, reqId) {
         if (win.__XHRShimAliases[reqId]){
